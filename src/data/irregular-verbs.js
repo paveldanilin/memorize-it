2e7c3087ce81a10c4bde7a00df94dd2fd923e091
Danilin <Pavel.Danilin@billing.ru>
2019-10-10 10:46:01 +1000

    Add new irregular words

--- src/data/irregular-verbs.js
+++ src/data/irregular-verbs.js
@@ -112,6 +112,262 @@ const IrregularVerbs = [
         pastSimple: 'brought',
         pastParticiple: 'brought',
     },
+    {
+        locale: {
+            ru: 'строить, сооружать'
+        },
+        infinitive: 'build',
+        pastSimple: 'built',
+        pastParticiple: 'built',
+    },
+    {
+        locale: {
+            ru: 'гореть, пылать'
+        },
+        infinitive: 'burn',
+        pastSimple: 'burnt',
+        pastParticiple: 'burnt',
+    },
+    {
+        locale: {
+            ru: 'покупать'
+        },
+        infinitive: 'buy',
+        pastSimple: 'bought',
+        pastParticiple: 'bought',
+    },
+    {
+        locale: {
+            ru: 'ловить, схватить'
+        },
+        infinitive: 'catch',
+        pastSimple: 'caught',
+        pastParticiple: 'caught',
+    },
+    {
+        locale: {
+            ru: 'выбирать'
+        },
+        infinitive: 'choose',
+        pastSimple: 'chose',
+        pastParticiple: 'chosen',
+    },
+    {
+        locale: {
+            ru: 'приходить, приезжать'
+        },
+        infinitive: 'come',
+        pastSimple: 'came',
+        pastParticiple: 'come',
+    },
+    {
+        locale: {
+            ru: 'стоить (стоимость)'
+        },
+        infinitive: 'cost',
+        pastSimple: 'cost',
+        pastParticiple: 'cost',
+    },
+    {
+        locale: {
+            ru: 'резать'
+        },
+        infinitive: 'cut',
+        pastSimple: 'cut',
+        pastParticiple: 'cut',
+    },
+    {
+        locale: {
+            ru: 'делать, выполнять, действовать'
+        },
+        infinitive: 'do',
+        pastSimple: 'did',
+        pastParticiple: 'done',
+    },
+    {
+        locale: {
+            ru: 'копать'
+        },
+        infinitive: 'dig',
+        pastSimple: 'dug',
+        pastParticiple: 'dug',
+    },
+    {
+        locale: {
+            ru: 'рисовать, тащить'
+        },
+        infinitive: 'draw',
+        pastSimple: 'drew',
+        pastParticiple: 'drawn',
+    },
+    {
+        locale: {
+            ru: 'мечтать'
+        },
+        infinitive: 'dream',
+        pastSimple: 'dreamt',
+        pastParticiple: 'dreamt',
+    },
+    {
+        locale: {
+            ru: 'пить'
+        },
+        infinitive: 'drink',
+        pastSimple: 'drank',
+        pastParticiple: 'drunk',
+    },
+    {
+        locale: {
+            ru: 'водить машину, ехать'
+        },
+        infinitive: 'drive',
+        pastSimple: 'drove',
+        pastParticiple: 'driven',
+    },
+    {
+        locale: {
+            ru: 'есть'
+        },
+        infinitive: 'eat',
+        pastSimple: 'ate',
+        pastParticiple: 'eaten',
+    },
+    {
+        locale: {
+            ru: 'падать'
+        },
+        infinitive: 'fall',
+        pastSimple: 'fell',
+        pastParticiple: 'fallen',
+    },
+    {
+        locale: {
+            ru: 'кормить'
+        },
+        infinitive: 'feed',
+        pastSimple: 'fed',
+        pastParticiple: 'fed',
+    },
+    {
+        locale: {
+            ru: 'чувствовать'
+        },
+        infinitive: 'feel',
+        pastSimple: 'felt',
+        pastParticiple: 'felt',
+    },
+    {
+        locale: {
+            ru: 'драться, сражаться, воевать'
+        },
+        infinitive: 'fight',
+        pastSimple: 'fought',
+        pastParticiple: 'fought',
+    },
+    {
+        locale: {
+            ru: 'находить, обнаруживать, встречать'
+        },
+        infinitive: 'find',
+        pastSimple: 'found',
+        pastParticiple: 'found',
+    },
+    {
+        locale: {
+            ru: 'лететь'
+        },
+        infinitive: 'fly',
+        pastSimple: 'flew',
+        pastParticiple: 'flown',
+    },
+    {
+        locale: {
+            ru: 'забывать'
+        },
+        infinitive: 'forget',
+        pastSimple: 'forgot',
+        pastParticiple: 'forgotten',
+    },
+    {
+        locale: {
+            ru: 'прощать'
+        },
+        infinitive: 'forgive',
+        pastSimple: 'forgave',
+        pastParticiple: 'forgiven',
+    },
+    {
+        locale: {
+            ru: 'замораживать, замерзать'
+        },
+        infinitive: 'freeze',
+        pastSimple: 'froze',
+        pastParticiple: 'frozen',
+    },
+    {
+        locale: {
+            ru: 'доставать, получать, добиваться'
+        },
+        infinitive: 'get',
+        pastSimple: 'got',
+        pastParticiple: 'got',
+    },
+    {
+        locale: {
+            ru: 'давать, отдавать'
+        },
+        infinitive: 'give',
+        pastSimple: 'gave',
+        pastParticiple: 'given',
+    },
+    {
+        locale: {
+            ru: 'идти, ехать, двигаться'
+        },
+        infinitive: 'go',
+        pastSimple: 'went',
+        pastParticiple: 'gone',
+    },
+    {
+        locale: {
+            ru: 'расти, увеличиваться'
+        },
+        infinitive: 'grow',
+        pastSimple: 'grew',
+        pastParticiple: 'grown',
+    },
+    {
+        locale: {
+            ru: 'иметь, обладать'
+        },
+        infinitive: 'have',
+        pastSimple: 'had',
+        pastParticiple: 'had',
+    },
+    {
+        locale: {
+            ru: 'слышать'
+        },
+        infinitive: 'hear',
+        pastSimple: 'heard',
+        pastParticiple: 'heard',
+    },
+    {
+        locale: {
+            ru: 'прятать, скрывать'
+        },
+        infinitive: 'hide',
+        pastSimple: 'hid',
+        pastParticiple: 'hidden',
+    },
+    {
+        locale: {
+            ru: 'ударять'
+        },
+        infinitive: 'hit',
+        pastSimple: 'hit',
+        pastParticiple: 'hit',
+    },
 ];
 
 export { IrregularVerbs };
